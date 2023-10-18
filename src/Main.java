404ac1f784b1cb4033e6fa39325464f8b7c06a34
Nafisa Kibria <nafisa21@iut-dhaka.edu>
2023-10-18 23:53:52 +0600

    modified main class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,51 @@
 public class Main {
     public static void main(String[] args) {
 
+        CompleteGraph completeGraph = new CompleteGraph(5);
+
+        completeGraph.removeVertex(1);
+        completeGraph.removeVertex(2);
+        completeGraph.addVertex();
+
+        completeGraph.GraphRepresentation();
+
+
+
+        ConnectedGraph c1=new ConnectedGraph(5);
+
+
+        c1.removeVertex(2);
+
+        c1.addVertex(2);
+        c1.addVertex(2);
+
+        c1.addEdge(2,4);
+        c1.addEdge(3,0);
+
+
+        c1.GraphRepresentation();
+
+
+        DisconnectedGraph DisconnectedGraph1 =new DisconnectedGraph(4);
+        DisconnectedGraph1.addEdge(0,1);
+        DisconnectedGraph1.addEdge(0,3);
+        DisconnectedGraph1.addEdge(2,3);
+        DisconnectedGraph1.addEdge(1,3);
+
+        DisconnectedGraph1.addVertex();
+        DisconnectedGraph1.addEdge(1,4);
+        DisconnectedGraph1.addVertex();
+
+        DisconnectedGraph1.GraphRepresentation();
+        DisconnectedGraph1.removeVertex(3);
+        DisconnectedGraph1.addVertex();
+        DisconnectedGraph1.addEdge(6,5);
+        DisconnectedGraph1.addEdge(3,5);
+        DisconnectedGraph1.addEdge(3,6);
+        DisconnectedGraph1.addEdge(6,4);
+
+
+        DisconnectedGraph1.GraphRepresentation();
         WeightedGraph graph = new WeightedGraph(6);
 
         graph.addEdge(0, 1, 4);
