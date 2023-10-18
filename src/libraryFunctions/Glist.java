214140ab6ad7abddfa35526abe223afc1cb29be2
Nafisa Kibria <nafisa21@iut-dhaka.edu>
2023-10-18 21:38:16 +0600

    added a method called toSTring.this method controls how the contents of the linked list are presented while printing a Glist object

--- src/libraryFunctions/Glist.java
+++ src/libraryFunctions/Glist.java
@@ -2,25 +2,29 @@ package libraryFunctions;
 
 public class Glist<E> {
     Node<E> head;
+    int[] valuearray= new int[1000];
+
 
     public void clear() {
         head = null;
 
     }
     public boolean add(E value) {
+        Node<E> newNode = new Node<>(value);
 
-        Node<E> current = head;
-        while (current.next!=null) {
+        if (head == null) {
 
-            current = current.next;
+            head = newNode;
+        } else {
+            Node<E> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+
+            current.next = newNode;
         }
 
-        Node newNode = new Node(value);
-
-        current.next= newNode;
         return true;
-
-
     }
     public boolean contains(E value)
     {
@@ -42,6 +46,27 @@ public class Glist<E> {
         return false;
 
     }
+    public String toString() {
+        if (head == null) {
+            return "Empty List";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+
+        Node<E> current = head;
+        while (current != null) {
+            sb.append(current.data);
+            current = current.next;
+            if (current != null) {
+                sb.append(", ");
+            }
+        }
+
+        sb.append("]");
+        return sb.toString();
+    }
+
     public int length()
     {
         int size=0;
@@ -51,5 +76,32 @@ public class Glist<E> {
         }
         return size;
 
+    }
+    public  void sort()
+    {
+        Node current = head;
+        int n=0;
+        while (current.next != null) {
+            valuearray[n]= (int) current.data;
+            current=current.next;
+            n++;
+        }
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if ((Integer)valuearray[j] > valuearray[j + 1]) {
+                    int temp = valuearray[j];
+                    valuearray[j] = valuearray[j + 1];
+                    valuearray[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+
+            if (!swapped) {
+                break;
+            }
+        }
     }
 }
